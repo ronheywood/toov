5352c07765468344763afa31e80ed0d6ca906f40
Ron Heywood <ron@ronheywood.co.uk>
2017-01-27 16:38:18 +0000

    Auth redirect uri calculated based on current host

--- public/js/AccountController.js
+++ public/js/AccountController.js
@@ -14,9 +14,12 @@
 
 		   		var authPath = 'https://login.eveonline.com/oauth/authorize/';
 		   		var response_type = 'response_type=code';
-				var redirect_uri = 'redirect_uri=http://localhost:8000/openauth';
+				
+				var base_server = '%s//%s'.format(window.location.protocol,window.location.host);
+				var redirect_uri = 'redirect_uri='+ base_server +'/openauth';
+
 				var client_id = 'client_id=4344fa43fd9d4114aa8823700af948c1';
-				var scope = 'scope=publicData+characterFittingsWrite+characterAssetsRead+esi-killmails.read_killmails.v1+esi-assets.read_assets.v1';
+				var scope = 'scope=publicData+characterAssetsRead+esi-assets.read_assets.v1';
 				var action = authPath + '?%s&%s&%s&%s&%s'.format(response_type,redirect_uri,client_id,scope)
 
 				var config = "toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=0,width=350,height=410,left=50,top=50,titlebar=yes";
