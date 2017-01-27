daab163e10595ae7b13a02e05b9431df5ff4738f
Ron Heywood <ron@ronheywood.co.uk>
2017-01-27 12:44:06 +0000

    Remove close interface on the login modal - it cannot be dismissed

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -32,14 +32,10 @@
 			    <!-- Modal content-->
 			    <div class="modal-content">
 			      <div class="modal-header">
-			        <button type="button" class="close" data-dismiss="modal">&times;</button>
 			        <h4 class="modal-title">Login with your Eve Account</h4>
 			      </div>
 			      <div class="modal-body">
 			        <img src="https://images.contentful.com/idjq7aai9ylm/4fSjj56uD6CYwYyus4KmES/4f6385c91e6de56274d99496e6adebab/EVE_SSO_Login_Buttons_Large_Black.png?w=270&h=45" alt="Sign In" class="btn" ng-click="authenticate()"/>
-			      </div>
-			      <div class="modal-footer">
-			        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 			      </div>
 			    </div>
 
